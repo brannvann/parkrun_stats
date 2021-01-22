e0899f45497208b1efde09a88554a25ab3e6f555
Yuriy Volodin <vjv@inbox.ru>
2021-01-22 16:51:21 +0300

    update script according to new html sourse

--- parkrun_volunteers.sh
+++ parkrun_volunteers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# статистика волонетров на забегах паркран
+echo "Сбор статистики о волонтерах на забегах паркран"
 
 # если скрипт запускается без параметров, то считается статистика паркрана Королёв
 # если задан параметр командной строки, то он используется в качестве имени забега
@@ -12,11 +12,20 @@ if [[ -n "$1" ]]; then
 	parkrun=$1
 fi
 
+function ProgressBar {
+  _progress=$(((${1}*10000/${2})/100))
+	_done=$((_progress*4/10))
+	_left=$((40-_done))
+	_done=$(printf "%${_done}s")
+	_left=$(printf "%${_left}s")
+  printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
+}
+
 declare -A countMap
 
 result_page='https://www.parkrun.ru/'$parkrun'/results/weeklyresults/?runSeqNumber='
 user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'
-total=`curl -s -A "$user_agent" $result_page | awk -F"Проведено забегов: " '{print $2}' | awk -F"</div>" '{print $1}'`
+total=$(curl -s -A "$user_agent" $result_page | awk -F'Мероприятий: <span class="num">' '{print $2}' | awk -F'</span>' '{print $1}' | tr -d '\n')
 
 echo "Паркран "$parkrun". Всего забегов: "$total
 
@@ -24,42 +33,42 @@ echo "Паркран "$parkrun". Всего забегов: "$total
 totalresult=$parkrun'_all_results.html'
 echo -n > $totalresult
 
-# подробная статистика волонетров по забегу
+# подробная статистика волонтеров по забегу
 full_stat=$parkrun'_full_stat.txt'
 echo -n > $full_stat
 
-for(( event_index=1; event_index<=$total; event_index++ ))
+for(( event_index=1; event_index<=total; event_index++ ))
 do
+  ProgressBar $event_index $total
 	# скачиваем содержимое страницы с результатами забега 
 	page_url=$result_page$event_index
-	echo "обработка" $page_url
-	page=`curl -s -A "$user_agent" $page_url`
+	page=$(curl -s -A "$user_agent" $page_url)
 
 	# сохранение результатов забега в файл ( на всякий случай )
-	echo $page >> $totalresult
+  #	echo $page >> $totalresult
 	
-	Volunteers=`echo $page | awk -F"благодаря которым состоялся этот забег:" '{print $2}' | awk -F"</p>" '{print $1}'`
-	vCount=`echo $Volunteers | awk -F"</a>," '{print NF}'`
-	echo "забег №" $event_index "количество волонтеров:" $vCount
-	for(( i=1; i<=$vCount; ++i ))
+	Volunteers=$(echo $page | awk -F"Спасибо нашим волонтёрам!</h3>" '{print $2}' | awk -F"</h3>" '{print $1}')
+	vCount=$(echo $Volunteers | awk -F"</a>," '{print NF}')
+	for(( i=1; i<=vCount; ++i ))
 	do
-		Volunteer=`echo $Volunteers | awk -F"</a>," '{print $'"$i"'}'`         
-		athleteId=`echo $Volunteer | awk -F"Number=" '{print $2}' | awk -F"'>" '{print $1}'`
-		Name=`echo $Volunteer | awk -F">" '{print $2}' | awk -F"<" '{print $1}'`
+		Volunteer=$(echo $Volunteers | awk -F"</a>" '{print $'"$i"'}')
+		athleteId=$(echo $Volunteer | awk -F"Number=" '{print $2}' | awk -F"'>" '{print $1}')
+		Name=$(echo $Volunteer | awk -F"'>" '{print $2}')
 		key="A"$athleteId" "$Name
-		echo -e $parkrun "\t" $event_index "\t" $key >> $full_stat 
-		
+		echo -e $parkrun"\t"$event_index"\t"$key >> $full_stat
+
 		value=${countMap[$key]}
-		value=$(expr $value + 1)
+		value=$((value + 1))
 		countMap[$key]=$value
 	done	
-	sleep 1
+	sleep 0.2
 done
+echo ""
 
-#вывод списка волонтеров с сортировкой по количеству забегов
+# вывод списка волонтеров с сортировкой по количеству забегов
 echo "================== Волонтеры забега "$parkrun" =================="
 for K in "${!countMap[@]}"; do echo $K ${countMap[$K]}; done | sort -rn -k4
 
-#вывод списка волонтеров в файл
+# вывод списка волонтеров в файл
 volunteers_file='volunteers_'$parkrun'.txt'
 for K in "${!countMap[@]}"; do echo $K ${countMap[$K]}; done | sort -rn -k4 >> $volunteers_file
